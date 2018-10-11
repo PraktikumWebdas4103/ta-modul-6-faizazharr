b62df551a71c68ea11bf6a9d3cef23d4aa8707a4
faizazharr <43517978+faizazharr@users.noreply.github.com>
2018-10-11 23:50:40 +0700

    Add files via upload

---
+++ daftarpostingan.php
@@ -0,0 +1,24 @@
+<center>
+		<table border="1">
+			<tr>
+				<td>
+					<a href="halamanawal.php">HOME</a>
+				</td>
+				<td>
+					<a href="posting.php">posting</a>
+				</td>
+				<td>
+					<a href="daftarpostingan.php">daftar postingan</a>
+				</td>
+				<td>
+					<a href="semuaposting.php">Semua posting</a>
+				</td>
+				<td>
+					<a href="editprofile.php">edit profile</a>
+				</td>
+				<td>
+					<a href="logout.php">logout</a>
+				</td>
+			</tr>
+		</table>
+	</center>
